79f81c76d26fd9400a4bcde610471445f7ea0618
Abbas Leenud <152507598+bqsst@users.noreply.github.com>
2024-11-26 14:28:11 +0700

    add soem test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+   return (
+      <div>
+         <CssBaseline />
+         <Button>
+            This is button
+         </Button>
+      </div>
+   )
 }
 
 export default App;
